5ec461764bfe8b3ccd0fbd33ae38943c699c5668
Shibly <shibly.phy@gmail.com>
2024-05-06 20:02:37 +0600

    Done with basic database schema and Faker factories

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Project;
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        // User::factory(10)->create();
+
+        User::factory()->create([
+            'name' => 'Mahmudur Rahman',
+            'email' => 'shibly.phy@gmail.com',
+            'password' => bcrypt('123456789'),
+            'email_verified_at' => time()
+        ]);
+
+        Project::factory()->count(60)->hasTasks(60)->create();
+    }
+}
